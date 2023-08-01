ff6b8efa921d974ed467cd6a7e05e118ca68bc92
vkvaibhavtb <vkvaibhav36@gmail.com>
2023-08-01 21:00:12 +0530

    Create charset.js

---
+++ charset.js
@@ -0,0 +1,22 @@
+let x;
+x =0;
+console.log(x);
+
+x=0.1;
+console.log(x)
+
+x = 'Hello world from vrs ';
+
+console.log(x)
+
+x = true;
+console.log(x)
+
+x = false;
+console.log(x);
+
+x= null;
+console.log(x);
+
+x = undefined;
+console.log(x);
